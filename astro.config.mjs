a3393458d1a2e1378f8ffad5337c20f71f58289a
Casper Engelmann <c.engelmann@custimy.io>
2023-12-12 15:56:33 +0100

    Update Astro config and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
-  integrations: [tailwind()]
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [tailwind()],
 });
